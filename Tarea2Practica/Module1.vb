01344740b518d3285cd83177ea81631fa7604587
Equipo <Equipo@DESKTOP-EAD5GPH>
2024-08-08 22:17:44 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2Practica/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.Write("Por favor, ingresa un número entero: ")
+        Dim numero As Integer = Integer.Parse(Console.ReadLine())
+
+
+        For i As Integer = 1 To 5
+            Dim siguienteNumero As Integer = numero + i
+
+
+            Dim tipo As String
+            If siguienteNumero Mod 2 = 0 Then
+                tipo = "par"
+            Else
+                tipo = "impar"
+            End If
+
+
+            Console.WriteLine(siguienteNumero & " es " & tipo)
+        Next
+
+
+        Console.WriteLine("Presiona cualquier tecla para salir")
+        Console.ReadKey()
+    End Sub
+
+End Module
